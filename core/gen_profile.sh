b274f7720897ccfe235c00ab482662dd7af73e94
rocket <rocket@5ba8dd8a-5c08-0410-8eb7-bf276fad51ba>
2006-01-11 22:29:23 +0000

    remove gen_processKernelModules.sh

--- core/gen_profile.sh
+++ core/gen_profile.sh
@@ -408,3 +408,21 @@ config_profile_dump() {
 	exit 0
 }
 
+cmdline_modules_register(){
+    local i data
+    data=$1
+    if [ "${data}" == "${data%:*}" ]
+    then
+        kernel_modules="${data}"
+        category="extra"
+    else
+        kernel_modules="${data#*:}"
+        category="${data%:*}"
+    fi
+
+    for i in $kernel_modules
+    do
+        profile_append_key "${category}" "${i}" "modules-cmdline"
+    done
+}
+
