e9308a818051d1b29852ca4c2fa11433149cb0d8
plasmaroo <plasmaroo@5ba8dd8a-5c08-0410-8eb7-bf276fad51ba>
2006-02-19 20:47:51 +0000

    Update manpage, clean up config options slightly.

--- core/gen_help_setup.sh
+++ core/gen_help_setup.sh
@@ -12,24 +12,24 @@ __register_config_option 'Internals' 'arch-override' 'true' 'false' 'Force to ar
 __register_config_option 'Internals' 'callback'	'true' 'false' 'Run the specified arguments after the kernel and modules have been compiled.'
 __register_config_option 'Internals' 'cachedir' 'true' 'false' 'Override the default cache location.'
 __register_config_option 'Internals' 'tempdir' 'true' 'false' "Location of Genkernel's temporary directory."
-__register_config_option 'Internals' 'log-override' 'true' 'false' '' # Hide
-__register_config_option 'Internals' 'postclear' 'false' 'true' 'Clear all temporary files and caches afterwards.'
 
 __register_config_option 'Internals' 'makeopts' 'true' 'false' 'Global make options.'
-__register_config_option 'Internals' 'usecolor' 'false' 'true' 'Use colored output.'
-profile_set_key usecolor true 'system'
-
 __register_config_option 'Internals' 'profile' 'true!m' 'false' 'Use specified profile(s).' 'config_profile_read'
 __register_config_option 'Internals' 'profile-dump' 'false' 'false' 'Dump the current profile to the cmdline.'
+__register_config_option 'Internals' 'usecolor' 'false' 'true' 'Use colored output.'
+profile_set_key usecolor true 'system'
 __register_config_option 'Internals' 'help' 'false' 'false' '' # Hidden.
+__register_config_option 'Internals' 'log-override' 'true' 'false' '' # Hidden.
 
 ## Cross Compilation
+__register_config_option 'Cross compile' 'cross-compile' 'true' 'false' 'Cross compiler settings (Overrides kernel-cross-compile and utils-cross-compile)'
 __register_config_option 'Cross compile' 'kernel-cross-compile' 'true' 'false' 'Kernel cross compiler settings.'
 __register_config_option 'Cross compile' 'utils-cross-compile' 'true' 'false' 'Utilities cross compiler settings.'
-__register_config_option 'Cross compile' 'cross-compile' 'true' 'false' 'Cross compiler settings (Overrides kernel-cross-compile and utils-cross-compile)'
 
 ## Kernel Config
 __register_config_option 'Kernel Configuration' 'kernel-config' 'true' 'false' 'Kernel configuration file to use for compilation.'
+__register_config_option 'Kernel Configuration' 'force-config' 'false' 'true' 'Turn on any config options genkernel deems mandatory.'
+profile_set_key force-config false 'system'
 __register_config_option 'Kernel Configuration'	'menuconfig'	 'false' 'true'	 'Run menuconfig after oldconfig.'
 profile_set_key 'menuconfig' false 'system'
 
@@ -48,11 +48,9 @@ __register_config_option 'Kernel Configuration' 'internal-initramfs' 'false' 'tr
 profile_set_key internal-initramfs false 'system'
 __register_config_option 'Kernel Configuration' 'kernel-tree'   'true' 'false' 'Location of kernel sources.'
 profile_set_key kernel-tree '/usr/src/linux'
-__register_config_option 'Kernel Configuration' 'kbuild-output'   'true' 'false' 'Location of kernel sources.'
+__register_config_option 'Kernel Configuration' 'kbuild-output'   'true' 'false' 'Location to use for Kbuild output.'
 __register_config_option 'Kernel Configuration' 'kernel-name' 'true' 'false' 'Tag the kernel and initramfs with a name; if not defined the option defaults to "genkernel".'
 profile_set_key kernel-name genkernel 'system'
-__register_config_option 'Kernel Configuration' 'force-config' 'false' 'true' 'Turn on any config options genkernel deems mandatory.'
-profile_set_key force-config false 'system'
 
 ## Initramfs options
 __register_config_option 'Initramfs' 'gmi' 'false' 'true' "Disable genkernel's initramfs scripts"
@@ -66,11 +64,10 @@ __register_config_option 'Initramfs' 'busybox-menuconfig' 'false' 'true' 'Run me
 __register_config_option 'Initramfs' 'udev' 'false' 'true' 'Add udev to the initramfs'
 profile_set_key udev true 'system'
 
+__register_config_option 'Initramfs' 'dmraid' 'false' 'true' 'Include DMRAID support.'
 __register_config_option 'Initramfs' 'evms2' 'false' 'true' 'Include EVMS2 support.'
 __register_config_option 'Initramfs' 'lvm2' 'false' 'true' 'Include LVM2 support.'
 __register_config_option 'Initramfs' 'e2fsprogs' 'false' 'true' 'Include e2fsprogs blkid support.'
-__register_config_option 'Initramfs' 'disklabel' 'false' 'true' 'Include disk label and uuid support in your initramfs.'
-
 __register_config_option 'Initramfs' 'kernel-modules'   'true!m' 'false' 'Add or subtract kernel modules from the initramfs. --kernel-module="GROUP:module -module"' 'cmdline_modules_register'
 __register_config_option 'Initramfs' 'kernel-modules-cpio' 'false' 'true' 'Add kernel modules to the initramfs'
 profile_set_key kernel-modules-cpio true 'system'
@@ -88,9 +85,9 @@ __register_config_option 'Initramfs' 'bladecenter' 'false' 'true' '' # Used by c
 __register_config_option 'Initramfs' 'unionfs' 'false' 'true' '' # Description empty, hide option
 
 ## ALL options
-__register_config_option '"all" target' 'initramfs' 'false' 'true' 'Build a initramfs'
+__register_config_option '"all::" target' 'initramfs' 'false' 'true' 'Build a initramfs'
 profile_set_key initramfs true 'system'
-__register_config_option '"all" target' 'install' 'false' 'true' 'Install the kernel to /boot after building; this does not change bootloader settings'
+__register_config_option '"all::" target' 'install' 'false' 'true' 'Install the kernel/initramfs to /boot after building.'
 profile_set_key install true 'system'
 
 ## Installation options
