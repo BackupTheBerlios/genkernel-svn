bd6be1452bc733d20a8f6e249a9a526bfff80c83
jean-francois <jean-francois@5ba8dd8a-5c08-0410-8eb7-bf276fad51ba>
2006-04-24 20:55:22 +0000

    Minor documentation fixes.

--- doc/genDoc.sh
+++ doc/genDoc.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 echo "Generating HTML..."
-asciidoc genkernel.8.txt
+asciidoc -b xhtml11 genkernel.8.txt
+asciidoc -b xhtml11 HOWTO-Genkernel-SSI.txt
 
+### TODO: Asciidoc 7.1+ has a manpage generation wrapper (see `a2x`)
+### but Asciidoc 7.1+ is not in portage yet
 echo "Generating manpage..."
 asciidoc -b docbook -d manpage genkernel.8.txt
 xsltproc genkernel.8.xsl genkernel.8.xml
