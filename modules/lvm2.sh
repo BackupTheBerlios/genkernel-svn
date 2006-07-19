d2258ad952cedd95acf43b3ff1b9d920f217ff50
plasmaroo <plasmaroo@5ba8dd8a-5c08-0410-8eb7-bf276fad51ba>
2006-07-19 19:35:42 +0000

    Remove BLK_DEV_DM_BBR check (not in mainline anymore it seems).

--- modules/lvm2.sh
+++ modules/lvm2.sh
@@ -3,7 +3,6 @@ require @pkg_lvm2-${LVM2_VER}:null:lvm2_compile
 cfg_register "BLK_DEV_DM" "REQUIRED for a fully functional lvm"
 cfg_register "DM_SNAPSHOT" "Recommended for a fully functional lvm"
 cfg_register "DM_MIRROR" "Recommended for a fully functional lvm"
-cfg_register "BLK_DEV_DM_BBR" "Recommended for a fully functional lvm"
 	
 lvm2::()
 {
