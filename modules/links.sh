d201a78db4d38e9f2851fd034e1ce1338f2716dc
plasmaroo <plasmaroo@5ba8dd8a-5c08-0410-8eb7-bf276fad51ba>
2006-10-08 20:16:52 +0000

    [PATCH] Link initramfs correctly, and also link System.map
    This patch fixes an incorrect location when using the --links option to
    create the symlink for initramfs. It also adds a symlink of System.map,
    to allow modules-update to run without errors on a diskless system
    without kernel sources.
    
    Thanks,
    Donnie

--- modules/links.sh
+++ modules/links.sh
@@ -21,13 +21,25 @@ links::()
 			ln -sf "${BOOTDIR}/kernel-${KV_FULL}" "${BOOTDIR}/kernel"
 		fi
 
+		# link to System.map
+		print_info 1 ">> Creating link to System.map"
+		if [ -n "$(profile_get_key install-path)" ]
+		then
+			print_info 1 ">> Creating link from $(profile_get_key install-path)/System.map-${KV_FULL} to $(profile_get_key install-path)/System.map"
+			ln -sf "$(profile_get_key install-path)/System.map-${KV_FULL}" "$(profile_get_key install-path)/System.map"
+
+		else
+			print_info 1 ">> Creating link from ${BOOTDIR}/System.map-${KV_FULL} to ${BOOTDIR}/System.map"
+			ln -sf "${BOOTDIR}/System.map-${KV_FULL}" "${BOOTDIR}/System.map"
+		fi
+
 		# link to the initramfs
 		if logicTrue $(external_initramfs)
 		then
 			if [ -n "$(profile_get_key install-initramfs-path)" ]
 			then
 				print_info 1 ">> Creating link from $(profile_get_key install-initramfs-path)/initramfs-${KV_FULL} to $(profile_get_key install-initramfs-path)/initramfs"
-				ln -sf "$(profile_get_key install-initramfs-path)/initramfs-${KV_FULL}" "$(profile_get_key initramfs-output)/initramfs"
+				ln -sf "$(profile_get_key install-initramfs-path)/initramfs-${KV_FULL}" "$(profile_get_key install-initramfs-path)/initramfs"
 			else
 				print_info 1 ">> Creating link from ${BOOTDIR}/initramfs-${KV_FULL} to ${BOOTDIR}/initramfs"
 				ln -sf "${BOOTDIR}/initramfs-${KV_FULL}" "${BOOTDIR}/initramfs"
